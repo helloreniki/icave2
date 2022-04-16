e7e6c988a07f5c99979d295177ad545d32a540af
Reniki <hello@reniki.com>
2022-04-16 17:47:46 +0200

    user seeder, pa db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+          // MANUALLY ADD USERS (super = true for statamic)
+          $user = new User();
+          $user->name = 'Renata';
+          $user->email = 'hello@reniki.com';
+          $user->password = \Hash::make('skrajnicas');
+          $user->super = true;
+          $user->save();
+
     }
 }
