52eee18e2fdb5e523cf6dabfd573674a1158d1d1
Reniki <hello@reniki.com>
2022-04-17 10:13:54 +0200

    tailwind create tailwind.config:    npx tailwindcss init    npx tailwindcss init tailwind.full.js --full tailwind.config    add content webpack.mix app.css: import tailwind layout: import app.css <link href="{{ mix('css/app.css') }}" rel="stylesheet"> yarn watch
    check if it works

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,36 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix
+ // .js('resources/js/app.js', 'public/js')
+
+ .postCss('resources/css/app.css', 'public/css')
+ .options({
+   postCss: [
+     require("tailwindcss"),
+   ]
+ })
+
+ // .postCss('resources/css/app.css', 'public/css', [
+ //   require('tailwindcss'),
+ // ])
+
+ .browserSync({
+   proxy: "http://icave2.test",
+   files: [
+     // 'public/js/app.js',
+     'public/css/app.css',
+     './resources/views/**/*.blade.php',
+   ],
+   // reloadDelay: 500, // wait one second before reloading
+   ghostMode: true, // switch all sync off (clicks, forms, scroll)
+   reloadOnRestart: true, // reload all browsers on restart
+   host: "192.168.1.10", // if you know your url already
+   notify: false, // no grey box notifications
+   ui: false, // disable 3001 ui page
+   online: true, // if you know you have internet connection
+   open: false // don't open browser automatically
+ })
+ .version()
+ .disableNotifications()
+;
