79270557cf25f09ee26af62cca1ed3b3254b5eb3
Reniki <hello@reniki.com>
2022-04-17 16:13:56 +0200

    basic structure, disable routes, 1st collection base: layout parts: header, footer collection Pages:    template, layout: base/layout, orderable, expect root page entry: Domov

---
+++ content/collections/pages/domov.md
@@ -0,0 +1,8 @@
+---
+id: a8895c33-3054-4260-ba6e-0c9a5dd266c9
+blueprint: pages
+title: Domov
+updated_by: 1
+updated_at: 1650204374
+---
+domov content
---
+++ resources/views/base/layout.blade.php
@@ -0,0 +1,39 @@
+@php
+  // PAGE —————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+@endphp
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Fonts -->
+  <link rel="preload" as="font" type="font/woff2" href="{{ url('/') }}/fonts/lato-v22-latin-ext_latin-700.woff2" crossorigin>
+  <link rel="preload" as="font" type="font/woff2" href="{{ url('/') }}/fonts/lato-v22-latin-ext_latin-900.woff2" crossorigin>
+  <link rel="preload" as="font" type="font/woff2" href="{{ url('/') }}/fonts/lato-v22-latin-ext_latin-regular.woff2" crossorigin>
+  <link rel="preload" as="font" type="font/woff2" href="{{ url('/') }}/fonts/parisienne-v12-latin-ext_latin-regular.woff2" crossorigin>
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <style>
+    [x-cloak] { display: none !important; }
+  </style>
+  <title>Document</title>
+</head>
+<body class="font-sans">
+
+  @include('parts.header')
+
+  <div>{{ $title }}</div>
+  <div>{!! $content !!}</div>
+
+  {{-- @foreach ($pb as $block)
+      @include('pb.' . $block['type'])
+  @endforeach --}}
+
+  @include('parts.footer')
+
+</body>
+</html>
