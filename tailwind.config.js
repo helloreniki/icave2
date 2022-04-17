8c28d296a82d1bf449d5449829a031652568ec62
Reniki <hello@reniki.com>
2022-04-17 14:27:03 +0200

    Fonts https://google-webfonts-helper.herokuapp.com/fonts    choose font, latin ext., select styles, modern browsers,    choose prefix /fonts, copy css to fonts.css app.css: @import 'fonts.css' fonts.css: copy css from webpage dl fonts to public/fonts/ base layout:   <link rel="preload" as="font" type="font/woff2" href="{{ url('/') }}/fonts/lato-v22-latin-ext_latin-700.woff2" crossorigin> tailwind.config add font: apply font in view
    check if it works (temporarily enable route for welcome page)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     ],
     theme: {
       extend: {},
+      fontFamily: {
+        sans: ['Lato', 'ui-sans-serif', 'Helvetica', 'sans-serif'], // 400 - regular, 700, 900
+        special: ['Parisienne'], // 400 - regular
+      },
     },
     plugins: [],
   }
