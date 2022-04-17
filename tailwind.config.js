8414ca2ff020ad866d598004f787ac0ddee824e9
Reniki <hello@reniki.com>
2022-04-17 15:18:09 +0200

    tailwind add colors, xs screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: [
       "./resources/**/*.blade.php",
@@ -9,6 +12,21 @@ module.exports = {
       fontFamily: {
         sans: ['Lato', 'ui-sans-serif', 'Helvetica', 'sans-serif'], // 400 - regular, 700, 900
         special: ['Parisienne'], // 400 - regular
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.trueGray, // blueGray, coolGray, gray, trueGray, warmGray
+
+        gold: '#c59d5f',
+        beige: '#f4f1ea',
+        grayish: '#505050', // dark gray
+      },
+      screens: {
+        'xs': '460px',
+        ...defaultTheme.screens,
       },
     },
     plugins: [],
