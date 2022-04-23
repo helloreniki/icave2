e1d218ae0ca90e628fd45dc09324ab42aae387c4
Reniki <hello@reniki.com>
2022-04-23 20:58:18 +0200

    hover with Alpine

--- resources/views/pb/jedilni_list.blade.php
+++ resources/views/pb/jedilni_list.blade.php
@@ -2,10 +2,10 @@
     // dd($jedilnik->kategorije)
 @endphp
 
-<div class="max-w-5xl mx-auto py-24 px-16 text-center bg-beige">
+<div class="max-w-5xl mx-auto py-24 px-16 text-center bg-beige my-4">
   <h2 class="font-special text-5xl xs:text-7xl text-gold mb-6 xs:mb-12 py-2 text-center">Jedilni list</h2>
 
-  <div class="space-y-16">
+  <div class="space-y-16" x-data="{ trenutnaJed: '' }">
     @foreach ($jedilnik->kategorije as $kategorija)
       <div class="">
         <h2 class="font-special text-4xl xs:text-6xl text-gold mb-8 py-2 col-span-12 text-center">{{ $kategorija->ime_kategorije }}</h2>
@@ -19,7 +19,9 @@
             <div></div>
           @endif
           @foreach ($kategorija->jedi as $jed)
-            <div class="flex flex-col @if($jed->cena) col-span-10 sm:col-span-11 @else col-span-8 sm:col-span-10 @endif">
+            <div class="select-none flex flex-col @if($jed->cena) col-span-10 sm:col-span-11 @else col-span-8 sm:col-span-10 @endif"
+              @mouseover="trenutnaJed = '{{ $jed->ime_jedi }}'"
+              >
               <div class="flex items-baseline group">
                 <div class="min-w-max text-lg font-bold tracking-wide group-hover:text-gold">{{ $jed->ime_jedi }}</div>
                 @if($jed->alergeni)<div class="hidden xs:block text-xs font-extralight ml-2">({{ $jed->alergeni }})</div>@endif
@@ -27,10 +29,11 @@
               </div>
               @if($jed->sestavine)<div class="text-left text-sm tracking-wide">{{ $jed->sestavine }}</div>@endif
             </div>
-            @if($jed->cena_mala)<div class="col-span-2 sm:col-span-1">{{ $jed->cena_mala }} €</div>@endif
-            @if($jed->cena_velika)<div class="col-span-2 sm:col-span-1">{{ $jed->cena_velika }} €</div>@endif
+            @if($jed->cena_mala)
+              <div class="col-span-2 sm:col-span-1" :class="trenutnaJed === '{{ $jed->ime_jedi }}' ? 'text-gold' : ''">{{ $jed->cena_mala }} €</div>@endif
+            @if($jed->cena_velika)<div class="col-span-2 sm:col-span-1" :class="trenutnaJed === '{{ $jed->ime_jedi }}' ? 'text-gold' : ''">{{ $jed->cena_velika }} €</div>@endif
             @if($jed->cena)
-            <div class="col-span-2 sm:col-span-1">{{ $jed->cena }} €
+            <div class="col-span-2 sm:col-span-1" :class="trenutnaJed === '{{ $jed->ime_jedi }}' ? 'text-gold' : ''">{{ $jed->cena }} €
             </div>
             @endif
           @endforeach
