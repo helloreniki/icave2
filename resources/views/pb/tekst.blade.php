462df99d75c5afbd1d7cfc8ab2a16402838b6882
Reniki <hello@reniki.com>
2022-04-27 19:50:59 +0200

    custom css for prose typography in tailwind.config

--- resources/views/pb/tekst.blade.php
+++ resources/views/pb/tekst.blade.php
@@ -2,7 +2,7 @@
     // dd($block->vsebina)
 @endphp
 
-<div class="max-w-7xl mx-auto py-16 md:py-32 px-12 text-center bg-{{$block->barva_ozadja}} prose prose-md md:prose-xl prose-a:text-gold prose-strong:text-gold">
+<div class="max-w-7xl mx-auto py-16 md:py-32 px-12 text-center bg-{{$block->barva_ozadja}} prose prose-md md:prose-xl">
   @foreach ($block->vsebina as $item)
     {{-- @dump($item->text) --}}
     @include('pb.tekst.' . $item->type)
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,16 @@ module.exports = {
       "./resources/**/*.vue",
     ],
     theme: {
-      extend: {},
+      extend: {
+        typography: theme => ({
+          DEFAULT: {
+            css: {
+              a: { color: theme('colors.gold'), '&:hover': { color: theme('colors.gold'), opacity: 0.8 }, },
+              // strong: { color: theme('colors.gold'), fontWeight: '700' },
+            },
+          },
+        }),
+      },
       fontFamily: {
         sans: ['Lato', 'ui-sans-serif', 'Helvetica', 'sans-serif'], // 400 - regular, 700, 900
         special: ['Parisienne'], // 400 - regular
